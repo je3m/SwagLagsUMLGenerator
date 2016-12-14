5ebf2ee37eeaa956e9e43a87bf170100bc77d43b
chris <christopher.malinosky@gmail.com>
2016-12-14 13:05:10 -0500

    Implemented fields for graphvis

--- src/application/GraphVizGraphReader.java
+++ src/application/GraphVizGraphReader.java
@@ -1,6 +1,11 @@
 package application;
 
+import java.util.List;
+
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import jdk.internal.org.objectweb.asm.Opcodes;
 
@@ -18,8 +23,31 @@ public class GraphVizGraphReader implements IGraphReader {
 			code += "label = \"{";
 			if((Opcodes.ACC_INTERFACE & c.access) != 0){
 				//is an interface
-			} else {
-				//not an interface
+				code += "\\<\\<interface\\>\\>\\n";
+			}
+			code += c.name + "|";
+			List<FieldNode> fields = c.fields;
+			for(FieldNode field: fields){
+				if((field.access & Opcodes.ACC_PUBLIC) > 0){
+					code += "+";
+				} else if((field.access & Opcodes.ACC_PRIVATE) > 0){
+					code += "-";
+				} else if((field.access & Opcodes.ACC_PROTECTED) > 0){
+					code += "#";
+				}
+				code+= " " + field.name + " : " + Type.getType(field.desc).getClassName() + "\\l";
+			}
+			code += "|";
+			List<MethodNode> methods = c.methods;
+			for(MethodNode method: methods){
+				if((method.access & Opcodes.ACC_PUBLIC) > 0){
+					code += "+";
+				} else if((field.access & Opcodes.ACC_PRIVATE) > 0){
+					code += "-";
+				} else if((field.access & Opcodes.ACC_PROTECTED) > 0){
+					code += "#";
+				}
+				code+= " " + field.name + " : " + Type.getType(field.desc).getClassName() + "\\l";
 			}
 		}
 
