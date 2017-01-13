782c9a63df933efcc25fdb2cd4bcddf3a77c6bc8
chris <christopher.malinosky@gmail.com>
2017-01-12 22:34:52 -0500

    Classes can't point to themselves

--- src/application/AssociationEdgeGenerator.java
+++ src/application/AssociationEdgeGenerator.java
@@ -11,22 +11,24 @@ public class AssociationEdgeGenerator implements IEdgeGenerator {
 	public void generateEdge(ProgramGraph pg) {
 		for(ClassNode node: pg.getNodes()){
 			for(ClassNode other: pg.getNodes()){
-				List<FieldNode> fields = node.fields;
-				for (FieldNode f: fields){
-					String sig = f.signature;
-
-					if(sig != null) {
-						for(String s :Utilities.getGenericTypes(sig)){
-							if(other.name.equals(Utilities.getClassPath(s))) {
-								pg.addEdge(new AssociationEdge(other, node, true));
+				if(!node.equals(other)){
+					List<FieldNode> fields = node.fields;
+					for (FieldNode f: fields){
+						String sig = f.signature;
+	
+						if(sig != null) {
+							for(String s :Utilities.getGenericTypes(sig)){
+								if(other.name.equals(Utilities.getClassPath(s))) {
+									pg.addEdge(new AssociationEdge(other, node, true));
+								}
 							}
 						}
+	
+						if((f.desc.length() > 1) && f.desc.substring(1, f.desc.length()-1).equals(other.name) ){
+							pg.addEdge(new AssociationEdge(other, node));
+						}
+	
 					}
-
-					if((f.desc.length() > 1) && f.desc.substring(1, f.desc.length()-1).equals(other.name) ){
-						pg.addEdge(new AssociationEdge(other, node));
-					}
-
 				}
 			}
 		}
--- src/application/DependencyEdgeGenerator.java
+++ src/application/DependencyEdgeGenerator.java
@@ -16,72 +16,74 @@ public class DependencyEdgeGenerator implements IEdgeGenerator {
 	public void generateEdge(ProgramGraph pg) {
 		for(ClassNode node: pg.getNodes()){
 			for(ClassNode other: pg.getNodes()){
-				List<MethodNode> methods = node.methods;
-				for(MethodNode mn: methods) {
-
-
-					//find one to many relationships with dependencies
-					if(mn.signature != null){
-						String argument = mn.signature.replace("(", "").split("\\)")[0];
-						String returnType = Utilities.getClassPath(mn.signature.replace("(", "").split("\\)")[1]);
-
-						//parsing some jank arguments
-						boolean split = true;
-						ArrayList<String> args = new ArrayList<String>();
-						args.add("");
-						for(int i = 0; i < argument.length(); i++){
-							if(argument.charAt(i) == '<'){
-								split = false;
-							} else if (argument.charAt(i) == '>'){
-								split = true;
-							} else if ((argument.charAt(i) == ';') && split){
-								args.add("");
+				if(!node.equals(other)){
+					List<MethodNode> methods = node.methods;
+					for(MethodNode mn: methods) {
+	
+	
+						//find one to many relationships with dependencies
+						if(mn.signature != null){
+							String argument = mn.signature.replace("(", "").split("\\)")[0];
+							String returnType = Utilities.getClassPath(mn.signature.replace("(", "").split("\\)")[1]);
+	
+							//parsing some jank arguments
+							boolean split = true;
+							ArrayList<String> args = new ArrayList<String>();
+							args.add("");
+							for(int i = 0; i < argument.length(); i++){
+								if(argument.charAt(i) == '<'){
+									split = false;
+								} else if (argument.charAt(i) == '>'){
+									split = true;
+								} else if ((argument.charAt(i) == ';') && split){
+									args.add("");
+								}
+	
+								if (!((argument.charAt(i) == ';') && split)) {
+									args.set(args.size() - 1, args.get(args.size()-1) +  argument.charAt(i));
+								}
+	
+	
 							}
-
-							if (!((argument.charAt(i) == ';') && split)) {
-								args.set(args.size() - 1, args.get(args.size()-1) +  argument.charAt(i));
+	
+							//checking though parameters for array lists contents
+							for (String s : args){
+								if(!s.equals("") && s.contains("<")){
+	
+									for (String sn : Utilities.getGenericTypes(s)){
+	
+										if(Utilities.getClassPath(sn).equals(Utilities.getClassPath(other.name))) {
+											pg.addEdge(new DependencyEdge(other, node, true));
+										}
+									}
+	
+								}
+	
 							}
-
-
-						}
-
-						//checking though parameters for array lists contents
-						for (String s : args){
-							if(!s.equals("") && s.contains("<")){
-
-								for (String sn : Utilities.getGenericTypes(s)){
-
-									if(Utilities.getClassPath(sn).equals(Utilities.getClassPath(other.name))) {
+	
+							if(returnType.contains("<")){
+								for (String s : Utilities.getGenericTypes(returnType)) {
+									if(Utilities.getClassPath(s).equals(Utilities.getClassPath(other.name))) {
 										pg.addEdge(new DependencyEdge(other, node, true));
 									}
 								}
-
-							}
-
-						}
-
-						if(returnType.contains("<")){
-							for (String s : Utilities.getGenericTypes(returnType)) {
-								if(Utilities.getClassPath(s).equals(Utilities.getClassPath(other.name))) {
-									pg.addEdge(new DependencyEdge(other, node, true));
-								}
 							}
 						}
-					}
-
-					for(Type t: Type.getArgumentTypes(mn.desc)) {
-						if(other.name.equals(Utilities.getClassPath(t))){
+	
+						for(Type t: Type.getArgumentTypes(mn.desc)) {
+							if(other.name.equals(Utilities.getClassPath(t))){
+								pg.addEdge(new DependencyEdge(other, node));
+							}
+						}
+						//					System.out.println(mn.name);
+						//					System.out.println(other.name + ":" + node.name);
+						//					System.out.println(Utilities.getClassPath(Type.getReturnType(mn.desc)));
+						if(other.name.equals(Utilities.getClassPath(Type.getReturnType(mn.desc)))){
+							//						System.out.println(other.name + ":" + node.name);
 							pg.addEdge(new DependencyEdge(other, node));
 						}
+						//					System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 					}
-					//					System.out.println(mn.name);
-					//					System.out.println(other.name + ":" + node.name);
-					//					System.out.println(Utilities.getClassPath(Type.getReturnType(mn.desc)));
-					if(other.name.equals(Utilities.getClassPath(Type.getReturnType(mn.desc)))){
-						//						System.out.println(other.name + ":" + node.name);
-						pg.addEdge(new DependencyEdge(other, node));
-					}
-					//					System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 				}
 			}
 		}
